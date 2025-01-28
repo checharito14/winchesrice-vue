b6796d30fae5a400ac7cfffa4b61f7f231701dcb
Cesar Rice <mc_rayorice@outlook.com>
2025-01-28 13:58:05 -0700

    mejorando seo, agregando sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import sitemap from "vite-plugin-sitemap";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        sitemap({
+            hostname: "https://winchesricemx.com",
+            routes: [
+                { path: "/home", changefreq: "monthly", priority: 0.8 },
+                { path: "/historia", changefreq: "monthly", priority: 0.6 },
+                { path: "/productos", changefreq: "monthly", priority: 0.9 },
+                { path: "/contacto", changefreq: "monthly", priority: 1.0 },   
+            ],
+        }),
+    ],
     server: {
-        host: '0.0.0.0',
-        port: 5173
+        host: "0.0.0.0",
+        port: 5173,
     },
     resolve: {
         alias: {
